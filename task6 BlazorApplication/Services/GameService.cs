0f688023e78a2f3a0e3d9fdeac06bbe3df02b346
Рустам Субханов <rustamsubhanov41@gmail.com>
2026-02-13 10:14:41 +0500

    added button for connecting to existing game

--- task6 BlazorApplication/Services/GameService.cs
+++ task6 BlazorApplication/Services/GameService.cs
@@ -4,5 +4,14 @@ namespace Services;
 
 public class GameService
 {
-    public Dictionary<string, GameRoom> Rooms { get; set; } = new();
+    public Dictionary<string, GameRoom> Rooms { get; set; } = [];
+
+    public List<GameRoom> GetWaitingRooms()
+    {
+        return Rooms.Values
+            .Where(r =>
+                (r.PlayerX != null && r.PlayerO == null) ||
+                (r.PlayerX == null && r.PlayerO != null))
+            .ToList();
+    }
 }
